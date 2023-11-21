e06566e75977c0a06ff1c55db5be711a014d5187
Ashraf_uddin <dev.fahim2021@gmail.com>
2023-11-21 10:53:42 +0600

    `Solved` Cannot find module "next/babel"

--- vlbd-frontend/src/Components/Layouts/Nav.jsx
+++ vlbd-frontend/src/Components/Layouts/Nav.jsx
@@ -1,15 +1,38 @@
+import Image from "next/image";
+import { BiLogoWhatsapp } from "react-icons/bi";
 import MainWrapper from "./MainWrapper";
 
+import logo from "../../../public/logo/vlbd_black.png";
 function Nav() {
-    return ( 
-        <>
-            <header>
-                <MainWrapper>
-                    <h1>Navigation</h1>
-                </MainWrapper>
-            </header>
-        </>
-     );
+
+  console.log(logo);
+  return (
+    <>
+      <header>
+        <MainWrapper>
+          <div className="header_inner">
+            <Image src={logo} alt="Vlbd Logo" width={100} />
+
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>Our Services</li>
+                <li>Visa Guide</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>News & Updates</li>
+              </ul>
+            </nav>
+
+            <button className="wta_btn">
+              <BiLogoWhatsapp />
+              <span>WhatsApp</span>
+            </button>
+          </div>
+        </MainWrapper>
+      </header>
+    </>
+  );
 }
 
 export default Nav;
